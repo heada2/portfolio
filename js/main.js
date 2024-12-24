6118a52a3d29829725a3c6ae4df609f991c010cc
heada2 <ria7780@naver.com>
2024-12-24 14:59:27 +0900

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  var page = $("#fullpage").fullpage({
-    autoScrolling : true,
-    responsiveWidth: 1024,
-    responsiveSlides: true,
-    fitToSection : false,
-    parallax : true,
-  });
-});
