e633fa26f51408a3b6cbe44f27b6e6ce862146bb
heada2 <ria7780@naver.com>
2025-02-18 23:34:57 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var page = $("#fullpage").fullpage({
+    autoScrolling : true,
+    responsiveWidth: 1024,
+    responsiveSlides: true,
+    fitToSection : false,
+    parallax : true,
+  });
+});
